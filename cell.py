35e3ae2da5a3d52f8cc5272800785e2ed4f94d2b
Diego Rocha <drochaesc@gmail.com>
2023-04-07 15:44:59 -0600

    Randomize mines using a staticmethod and setting the is_mine attribute of some random Cell objects to True

--- cell.py
+++ cell.py
@@ -1,14 +1,24 @@
 from tkinter import Button
+import random
+import settings
 
 class Cell:
-    def __init__(self, is_mine=False):
+    all = []
+    def __init__(self,x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
+        
+        # Append the object to the Cell.all List
+        Cell.all.append(self)
     
     def create_btn_object(self, location):
         btn = Button(
             location,
-            text='Text'
+            width=12,
+            height=4,
+            text=f"{self.x},{self.y}"
         )
         btn.bind('<Button-1>', self.left_click_actions) # Left Click
         btn.bind('<Button-3>', self.right_click_actions) # Right Click
@@ -21,3 +31,14 @@ class Cell:
     def right_click_actions(self, event):
         print(event)
         print("I am right clicked!")
+    
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(
+            Cell.all, settings.MINES_COUNT # 9 Cells will be converted into mines
+        )
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+    
+    def __repr__(self):
+        return f"Cell({self.x}, {self.y})"
--- main.py
+++ main.py
@@ -41,11 +41,13 @@ center_frame.place(
 
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
-        c = Cell()
+        c = Cell(x, y)
         c.create_btn_object(center_frame)
         c.cell_btn_object.grid( # Will take the center frame as the parent
             column=x,row=y
         )
+
+Cell.randomize_mines()
 
 # Run window
 root.mainloop()
